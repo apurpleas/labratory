2ce0aa8ee900279f5394f388c10faa6112fd82bb
apurpleas <alexv.av157@gmail.com>
2023-10-04 00:29:14 +0300

    fix triangle.py

--- triangle.py
+++ triangle.py
@@ -1,5 +1,7 @@
 
 def area(a, h):
+	'''Принимает числа a и h, возвращает a*h/2'''
 	return a * h / 2
 def perimeter(a, b, c):
+	'''Принимает числа a, b и c, возвращает их сумму'''
 	return a + b + c
