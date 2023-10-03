15c070398e1e62f1d40d30a6a81133426d960fbd
apurpleas <alexv.av157@gmail.com>
2023-10-04 00:27:36 +0300

    fix circle.py

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+	'''Принимает радиус r, возвращает r*r*3,1415926535'''
     return math.pi * r * r
 
 
 def perimeter(r):
+	'''Принимает радиус r, возвращает 2*r*3,1415926535'''
     return 2 * math.pi * r
 
